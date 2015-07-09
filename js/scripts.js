270e3bc0aec1a9afed8c18eceee38b93e7928fd2
Elizabeth Kipp <liz@elizabethkipp.com>
2015-07-09 14:57:52 -0500

    hides

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
 	console.log('ready');
 
-	$('.category').hide();
+	$('.category-cake').hide();
+
+	$('.category-flowers').hide();
+
+	$('.category-rings').hide();
+
+	$('.category-shoes').hide();
+
+	$('.category-dress').hide();
+
+
+	$('.category-locations').hide();
+
+	$('.category-details').hide();
+
+	$('.category-favors').hide();
+
 
 
 
