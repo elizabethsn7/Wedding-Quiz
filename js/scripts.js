4c02a5a44c44e44261ba3540e6ade0682283eb27
Elizabeth Kipp <liz@elizabethkipp.com>
2015-07-15 14:07:47 -0500

    wednesday session

--- js/scripts.js
+++ js/scripts.js
@@ -1,178 +1,180 @@
 
-
-
 $(document).ready(function() {
 	console.log('ready');
 
 
-	var category= [
+	var dresses = category.dress
+	'<h2>' + dresses.categoryTitle + '<h2>'
+	dresses.opt2
+
+	var category = {
 		
-		'dress' :[
+		'dresses' : {
+			'categoryTitle' : 'Dresses'
+			'opt1' :{
+				'imgURL' : '../images/dress_classic',
+				'description' : 'classic-dress'
+			},
+			'opt2':{
+                'imgURL': '../images/dress_modern',
+	            'description' : 'modern'
+
+			},
+			'opt3':{
+	           'imgURL': '../images/dress_shabby',
+	           'description' : 'shabby chic'
+	        },
+	        'opt4':{
+	           'imgURL': '../images/dress_glam',
+	           'description' : 'glam'
+	        }
+	    },
+
+		'rings' : {
+			'opt1' :{
+				'imgURL' : '../images/rings_classic',
+				'description' : 'classic'
+			},
+			'opt2':{
+	    	    'imgURL': '../images/rings_modern',
+	            'description' : 'modern'
+
+			},
+			'opt3':{
+	           	'imgURL': '../images/rings_shabby',
+	            'description' : 'shabby chic'
+	        },
+	        'opt4':{
+	        	'imgURL': '../images/rings_glam',
+	            'description' : 'glam'
+	        }
+		},
+
+
+		'ceremony' : {
 
 			'opt1' :{
-					'imgURL' : '../images/dress_classic',
-					'description' : 'classic-dress'
-				},
-				'opt2':{
-	                'imgURL': '../images/dress_modern',
-	                'description' : 'modern-dress'
+				'imgURL' : '../images/ceremony_classic',
+				'description' : 'classic'
+			},
+			'opt2':{
+	            'imgURL': '../images/ceremony_modern',
+	            'description' : 'modern'
 
-				},
-				'opt3':{
-	                'imgURL': '../images/dress_shabby',
-	                'description' : 'shabby-chic-dress'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/dress_glam',
-	                'description' : 'glam-dress'
-	            }
-	    ],
+			},
+			'opt3':{
+	           	'imgURL': '../images/ceremony_shabbychic',
+	            'description' : 'shabby chic'
+	        },
+	        'opt4':{
+	            'imgURL': '../images/ceremony_glam',
+                'description' : 'glam'
+	        }
+	    },
 
-		'rings' :[
+
+		'shoes' : {
 			'opt1' :{
-					'imgURL' : '../images/rings_classic',
-					'description' : 'classic-ring'
-				},
-				'opt2':{
-	                'imgURL': '../images/rings_modern',
-	                'description' : 'modern-ring'
+				'imgURL' : '../images/shoes_classic',
+				'description' : 'classic'
+			},
+			'opt2':{
+	       	    'imgURL': '../images/shoes_modern',
+	            'description' : 'modern'
 
-				},
-				'opt3':{
-	                'imgURL': '../images/rings_shabby',
-	                'description' : 'shabby-chic-ring'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/rings_glam',
-	                'description' : 'glam-ring'
-	            }
-	    ],
+			},
+			'opt3':{
+	       	    'imgURL': '../images/shoes_shabby',
+	            'description' : 'shabby chic'
+	        },
+	        'opt4':{
+	            'imgURL': '../images/shoes_glam',
+	            'description' : 'glam'
+	        }
+		},
 
-
-		'ceremony' :[
+		'flowers' : {
 			'opt1' :{
-					'imgURL' : '../images/ceremony_classic',
-					'description' : 'classic-ceremony'
-				},
-				'opt2':{
-	                'imgURL': '../images/ceremony_modern',
-	                'description' : 'modern-ceremony'
-
-				},
-				'opt3':{
-	                'imgURL': '../images/ceremony_shabbychic',
-	                'description' : 'shabby-chic-ceremony'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/ceremony_glam',
-	                'description' : 'glam-ceremony'
-	            }
-	    ],
-
-
-		'shoes' :[
-			'opt1' :{
-					'imgURL' : '../images/shoes_classic',
-					'description' : 'classic-shoes'
-				},
-				'opt2':{
-	                'imgURL': '../images/shoes_modern',
-	                'description' : 'modern-shoes'
-
-				},
-				'opt3':{
-	                'imgURL': '../images/shoes_shabby',
-	                'description' : 'shabby-shoes'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/shoes_glam',
-	                'description' : 'glam-shoes'
-	            }
-	    ],
-
-		'flowers' :[
-			'opt1' :{
-					'imgURL' : '../images/flower_classic',
-					'description' : 'classic-flowers'
-				},
-				'opt2':{
-	                'imgURL': '../images/flower_modern',
-	                'description' : 'mordern-flowers'
-
-				},
-				'opt3':{
+				'imgURL' : '../images/flower_classic',
+				'description' : 'classic'
+			},
+			'opt2':{
+	           'imgURL': '../images/flower_modern',
+	            'description' : 'mordern'
+			},
+			'opt3':{
 	                'imgURL': '../images/flower_shabby',
-	                'description' : 'shabby-chic-flowers'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/flower_glam',
-	                'description' : 'glam-flowers'
-	            }
-	    ],   
+	                'description' : 'shabby chic'
+	        },
+	        'opt4':{
+	        'imgURL': '../images/flower_glam',
+	        'description' : 'glam'
+            }
+		},   
 
 
-		'details' :[
+		'details' : {
 			'opt1' :{
-					'imgURL' : '../images/details_classic',
-					'description' : 'classic-details'
-				},
-				'opt2':{
-	                'imgURL': '../images/details_modern',
-	                'description' : 'modern-details'
+				'imgURL' : '../images/details_classic',
+				'description' : 'classic'
+			},
+			'opt2':{
+	            'imgURL': '../images/details_modern',
+	            'description' : 'modern'
 
-				},
-				'opt3':{
-	                'imgURL': '../images/details_shabby',
-	                'description' : 'shabby-chic-details'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/details_glam',
-	                'description' : 'glam-details'
-	            }
-	    ],
+			},
+			'opt3':{
+	            'imgURL': '../images/details_shabby',
+	            'description' : 'shabby chic'
+	        },
+	        'opt4':{
+	        	'imgURL': '../images/details_glam',
+	            'description' : 'glam'
+			}
+		},
 
 
-		'favors' :[
+		'favors' : {
 			'opt1' :{
-					'imgURL' : '../images/favors_classic',
-					'description' : 'classic-favors'
-				},
-				'opt2':{
-	                'imgURL': '../images/favors_modern',
-	                'description' : 'modern-favors'
+				'imgURL' : '../images/favors_classic',
+				'description' : 'classic'
+			},
+			'opt2':{
+	            'imgURL': '../images/favors_modern',
+	            'description' : 'modern'
 
-				},
-				'opt3':{
-	                'imgURL': '../images/favors_shabby',
-	                'description' : 'shabby-chic-favors'
-	            },
-	            'opt4':{
-	                'imgURL': '../images/favors_glam',
-	                'description' : 'glam-favors'
+			},
+			'opt3':{
+	            'imgURL': '../images/favors_shabby',
+	      	    'description' : 'shabby chic'
+	         },
+	         'opt4':{
+	           'imgURL': '../images/favors_glam',
+               'description' : 'glam'
 	            }
-	    ],
+	    },
 
 
-	    'cake' :[
+	    'cake' : {
 			'opt1' :{
 				'imgURL' : '../images/cake_classic.jpg',
-				'description' : 'classic-cake'
+				'description' : 'classic'
 			},
 			'opt2':{
                 'imgURL': '../images/cake_modern.jpg',
-                'description' : 'modern-cake'
+                'description' : 'modern'
 
 			},
 			'opt3':{
                 'imgURL': '../images/cake_shabby.jpg',
-                'description' : 'shabby-chic-cake'
+                'description' : 'shabby chic'
             },
             'opt4':{
                 'imgURL': '../images/cake_glam.jpg',
-                'description' : 'glam-cake'
+                'description' : 'glam'
             }
-         ]
-	];
+         }
+	};
 
 	$('.category').hide();
 
