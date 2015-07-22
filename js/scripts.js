45ccdee1052ffbf576efb0192591e2d4b002bc50
Elizabeth Kipp <liz@elizabethkipp.com>
2015-07-22 10:10:04 -0500

    opt1-4

--- js/scripts.js
+++ js/scripts.js
@@ -34,10 +34,22 @@ $(document).ready(function() {
   function callCategory() {
   	var categoryTitle = category.dresses.categoryTitle;
   	var opt1 = category.dresses.opt1.imgURL;
+  	var opt2 = category.dresses.opt2.imgURL;
+  	var opt3 = category.dresses.opt3.imgURL;
+  	var opt4 = category.dresses.opt4.imgURL;
 
   	$('.category').append('<h2>' + categoryTitle + '</h2>');
-  	$('.category').append('<img>' + opt1 + '</img>'); //psuedo code
+  	$('.category').append('<img>' + opt1 + '</img>'); //psuedo 
+  	console.log("options");
+  	$('.category').append('<img>' + opt2 + '</img>');
+  	$('.category').append('<img>' + opt3 + '</img>');
+  	$('.category').append('<img>' + opt4 + '</img>');
   	
+
+
+
+
+
   };
 
 	  
