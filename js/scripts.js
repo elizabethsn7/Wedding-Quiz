d9de357e827e6ee3c625f04c32a5d66610c49bb4
Elizabeth Kipp <liz@elizabethkipp.com>
2015-08-26 10:10:49 -0500

    start button

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
        $('.category').addClass('portrait');
     }
   }
-
+  // $('body').addClass('quiz-color').removeClass('pink');
 
   function callCategory() {
 
