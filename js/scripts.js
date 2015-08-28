697fb5a65d38cf5ea2900aa3a2de630c805c78e2
Elizabeth Kipp <liz@elizabethkipp.com>
2015-08-28 10:42:09 -0500

    classic to timeless and shabby to romantic

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,63 @@
 var categoryArray = ['start quiz', 'dresses', 'rings', 'ceremony', 'shoes', 'flowers', 'details', 'favors', 'cake'];
 
 
-var resultsImages = {
-  'dresses' : [
-    'dress1.jpg',
-    'dress2.jpg',
-    'dress3.jpg',
-    'dress4.jpg',
-  ]
-  'rings': [
-    'rings1.jpg',
-    'rings2.jpg',
-    'rings3.jpg',
-    'rings4.jpg',
-  ]  
-  'ceremony': [
-    'ceremony1.jpg',
-    'ceremony2.jpg',
-    'ceremony3.jpg',
-    'ceremony4.jpg',
-  ]
-  'shoes': [
-    'shoes1.jpg',
-    'shoes2.jpg',
-    'shoes3.jpg',
-    'shoes4.jpg',
-  ]
-  'flowers': [
-    'flowers1.jpg',
-    'flowers2.jpg',
-    'flowers3.jpg',
-    'flowers4.jpg',
-  ]
-  'details': [
-    'details1.jpg',
-    'details2.jpg',
-    'details3.jpg',
-    'details4.jpg',
-  ]
-  'favors': [
-    'favors.1',
-    'favors2.jpg',
-    'favors3.jpg',
-    'favors4.jpg',
-  ]
-  'cake': [
-    'cake1.jpg',
-    'cake2.jpg',
-    'cake3.jpg',
-    'cake4.jpg',
-  ]
-}
+// var resultsImages = {
+//   'dresses' : [
+//     'dress1.jpg',
+//     'dress2.jpg',
+//     'dress3.jpg',
+//     'dress4.jpg',
+//   ]
+//   'rings': [
+//     'rings1.jpg',
+//     'rings2.jpg',
+//     'rings3.jpg',
+//     'rings4.jpg',
+//   ]  
+//   'ceremony': [
+//     'ceremony1.jpg',
+//     'ceremony2.jpg',
+//     'ceremony3.jpg',
+//     'ceremony4.jpg',
+//   ]
+//   'shoes': [
+//     'shoes1.jpg',
+//     'shoes2.jpg',
+//     'shoes3.jpg',
+//     'shoes4.jpg',
+//   ]
+//   'flowers': [
+//     'flowers1.jpg',
+//     'flowers2.jpg',
+//     'flowers3.jpg',
+//     'flowers4.jpg',
+//   ]
+//   'details': [
+//     'details1.jpg',
+//     'details2.jpg',
+//     'details3.jpg',
+//     'details4.jpg',
+//   ]
+//   'favors': [
+//     'favors.1',
+//     'favors2.jpg',
+//     'favors3.jpg',
+//     'favors4.jpg',
+//   ]
+//   'cake': [
+//     'cake1.jpg',
+//     'cake2.jpg',
+//     'cake3.jpg',
+//     'cake4.jpg',
+//   ]
+// }
 
 var itemCategory;
 var result;
 
-var classicArray = [];
+var timelessArray = [];
 var modernArray  = [];
-var shabbyArray  = [];
+var romanticArray  = [];
 var glamArray = [];
 
 
@@ -92,23 +92,23 @@ $(document).ready(function() {
 
     //this var options
     var personalityTypes = {
-      'classic' : '_classic',
+      'timeless' : '_timeless',
       'modern'  : '_modern',
-      'shabby'  : '_shabby',
+      'romantic'  : '_romantic',
       'glam'    : '_glam'
     };
 
-    var classic = personalityTypes.classic;
+    var timeless = personalityTypes.timeless;
     var modern  = personalityTypes.modern;
-    var shabby  = personalityTypes.shabby;
+    var romantic  = personalityTypes.romantic;
     var glam    = personalityTypes.glam;
 
 
     $('.category').append('<form></form>');
 
-    $('.category form').append('<label><img src="images/' + itemCategory + classic + '.jpg"/><input type="radio" name="' + itemCategory + '" value="classic"></label>');
+    $('.category form').append('<label><img src="images/' + itemCategory + timeless + '.jpg"/><input type="radio" name="' + itemCategory + '" value="timeless"></label>');
     $('.category form').append('<label><img src="images/' + itemCategory + modern + '.jpg"/><input type="radio" name="' + itemCategory + '" value="modern"></label>');
-    $('.category form').append('<label><img src="images/' + itemCategory + shabby + '.jpg"/><input type="radio" name="' + itemCategory + '" value="shabby"></label>');
+    $('.category form').append('<label><img src="images/' + itemCategory + romantic + '.jpg"/><input type="radio" name="' + itemCategory + '" value="romantic"></label>');
     $('.category form').append('<label><img src="images/' + itemCategory + glam + '.jpg"/><input type="radio" name="' + itemCategory + '" value="glam"></label>');
     $('.category form').append('<input class="submit" type="submit" value="submit" style="display:none;" disabled>');
    }
@@ -134,20 +134,20 @@ function callResult() {
 
     console.log(selectedOption);
 
-    if (selectedOption === "classic") {
-      console.log('classic' + 'classic value');
-      classicArray.push('1');
-      //console.log(classicArray);
+    if (selectedOption === "timeless") {
+      console.log('timeless' + 'timeless value');
+      timelessArray.push('1');
+      //console.log(timelessArray);
     }
     else if (selectedOption === "modern") {
       console.log('modern' + 'modern value');
       modernArray.push('1');
       //console.log(modernArray);
     }
-    else if (selectedOption === "shabby") {
-      console.log('shabby' + 'shabby value');
-      shabbyArray.push('1');
-      console.log(shabbyArray);
+    else if (selectedOption === "romantic") {
+      console.log('romantic' + 'romantic value');
+      romanticArray.push('1');
+      console.log(romanticArray);
     }
     else if (selectedOption === "glam") {
       console.log('glam' + 'glam value');
@@ -155,9 +155,9 @@ function callResult() {
       //console.log(glamArray);
     }
 
-    console.log(classicArray);
+    console.log(timelessArray);
     console.log(modernArray);
-    console.log(shabbyArray);
+    console.log(romanticArray);
     console.log(glamArray);
 
 
@@ -166,24 +166,25 @@ function callResult() {
     $('.category').empty();
 
 
-    if ( classicArray.length >= modernArray.length && classicArray.length >= glamArray.length && classicArray.length >= glamArray.length) {
-      console.log('classicArray');
-      result = "classic"
+    if ( timelessArray.length >= modernArray.length && timelessArray.length >= romanticArray.length && timelessArray.length >= glamArray.length) {
+      console.log('timelessArray');
+      result = "Timeless"
       callResult();
       
     }
-    else if ( modernArray.length >= classicArray.length && modernArray.length >= glamArray.length && modernArray.length >= glamArray.length) {
+    else if ( modernArray.length >= timelessArray.length && modernArray.length >= glamArray.length && modernArray.length >= romanticArray.length) {
       console.log('modernArray');
-      result = "modern"
+      result = "Modern"
       callResult();
     }
-    else if ( shabbyArray.length >= modernArray.length && shabbyArray.length >= classicArray.length && shabbyArray.length >= glamArray.length) {
-      console.log('shabbyArray');
-      result = "shabby"
+    else if ( romanticArray.length >= modernArray.length && romanticArray.length >= timelessArray.length && romanticArray.length >= glamArray.length) {
+      console.log('romanticArray');
+      result = "Romantic"
+      callResult();
     }
-    else if ( glamArray.length >= modernArray.length && glamArray.length >= classicArray.length && glamArray.length >= shabbyArray.length) {
+    else if ( glamArray.length >= modernArray.length && glamArray.length >= timelessArray.length && glamArray.length >= romanticArray.length) {
       console.log('glamArray');
-      result = "glam"
+      result = "Glam"
       callResult();
       }
     }
@@ -204,7 +205,7 @@ function callResult() {
 
 // this code should determine the results
 
-//var resultsArray = [classic, modern, shabby, glam]
+//var resultsArray = [timeless, modern, romantic, glam]
 
 });
 
