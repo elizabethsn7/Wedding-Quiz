2dd9be40f435779d980f0a11ed9d400fd4d4ae29
Elizabeth Kipp <liz@elizabethkipp.com>
2015-09-24 16:42:59 -0500

    changed font to lato

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var requestURL = 'https://www.googleapis.com/youtube/v3/search';
+
+$(function() {
+    console.log('ready');
+
+    $('#search-term').submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+
+
+        function getRequest(searchTerm) {
+            $.getJSON('https://www.googleapis.com/youtube/v3/search' + searchTerm + '&r=json', function(data) {
+                getRequest(searchTerm);
+                console.log($(this).find('input').val());
+            });
+            var params = {
+                s: searchTerm,
+                r: 'json'
+            };
+            url = 'https://www.googleapis.com/youtube/v3/searchs';
+
+            $.getJSON(requestURL, params, function(data) {
+                value = data.val;
+                myData = data.Search;
+                showResults(data.Search);
+                $.each(myData, function(index, value) {
+                    console.log(value.Title);
+                });
+            });
+        };
+
+        $.getJSON('https://www.googleapis.com/youtube/v3/search', {
+                part: 'snippet',
+                key: 'AIzaSyD27ABFHFCuTG5zoQd_9D0Yx3KmUMY_TWI',
+                q: "",
+
+            },
+
+            function showResults(results) {
+                var html = "";
+                $.each(results, function(index, value) {
+                    html += '<p>' + value.Title + '</p>';
+
+                });
+                $('#search-results').html(html);
+
+            });
+
+    });
+});
